16964a3d176c9abda6b9ecad5307ded5d40ca14f
takahiro-shimizu <tkbchando@gmail.com>
2021-04-24 07:31:57 +0000

    ユーザ詳細ページ・プロフィール編集機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
  root 'post_images#index'
  resources :post_images, only: [:new, :create, :index, :show, :destroy]do
   resource :favorites, only: [:create, :destroy]
-
   resources :post_comments, only: [:create, :destroy]
  end
+ resources :users,only:[:show, :edit, :update]
 
 end
